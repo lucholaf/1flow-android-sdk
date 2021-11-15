b5a61343cf59f8e7d47819240b71181729e9ea54
amitskybox <amit@tryskybox.com>
2021-11-15 20:21:49 +0530

    - dummy commit for testing 18

--- app/src/main/java/com/oneflow/analytics/utils/NetworkChangeReceiver.java
+++ app/src/main/java/com/oneflow/analytics/utils/NetworkChangeReceiver.java
@@ -11,53 +11,53 @@ import com.oneflow.analytics.repositories.LogUserDBRepo;
 import com.oneflow.analytics.repositories.Survey;
 import com.oneflow.analytics.sdkdb.OneFlowSHP;
 
-public class NetworkChangeReceiver extends BroadcastReceiver implements MyResponseHandler{
+public class NetworkChangeReceiver extends BroadcastReceiver implements MyResponseHandler {
 
     public Context context;
+
     @Override
     public void onReceive(final Context context, final Intent intent) {
 
         this.context = context;
-       // int status = NetworkUtil.getConnectivityStatusString(context);
+        // int status = NetworkUtil.getConnectivityStatusString(context);
         //Helper.makeText(context,"OneFlow Receiver called ["+intent.getAction()+"]",1);
-        if ("android.net.conn.CONNECTIVITY_CHANGE".equals(intent.getAction())) {
+        Helper.v("NetworkChangeReceiver", "OneFlow network state changes["+Helper.isConnected(context)+"]");
 
-            if (Helper.isConnected(context)) {
-               // Helper.makeText(context,"Network available",1);
-                LocationResponse lr = new OneFlowSHP(context).getUserLocationDetails();
 
-                if(lr!=null) {
-                    checkOffLineSurvey();
-                }else{
-                    String projectKey = new OneFlowSHP(context).getStringValue(Constants.APPIDSHP);
-                    OneFlow.configure(context,projectKey);
-                   // CurrentLocation.getCurrentLocation(context,this,Constants.ApiHitType.fetchLocation);
-                }
+        if (Helper.isConnected(context)) {
+            // Helper.makeText(context,"Network available",1);
+            LocationResponse lr = new OneFlowSHP(context).getUserLocationDetails();
+
+            if (lr != null) {
+                checkOffLineSurvey();
             } else {
-               // Helper.makeText(context,"Network gone",1);
+                String projectKey = new OneFlowSHP(context).getStringValue(Constants.APPIDSHP);
+                OneFlow.configure(context, projectKey);
+                // CurrentLocation.getCurrentLocation(context,this,Constants.ApiHitType.fetchLocation);
             }
+
 
         }
     }
 
-    public void checkOffLineSurvey(){
-        LogUserDBRepo.fetchSurveyInput(context,this, Constants.ApiHitType.fetchSurveysFromDB);
+    public void checkOffLineSurvey() {
+        LogUserDBRepo.fetchSurveyInput(context, this, Constants.ApiHitType.fetchSurveysFromDB);
     }
 
     @Override
     public void onResponseReceived(Constants.ApiHitType hitType, Object obj, int reserve) {
 
-        switch(hitType){
+        switch (hitType) {
 
             case fetchSurveysFromDB:
-                SurveyUserInput survey = (SurveyUserInput)obj;
-                if(survey!=null) {
+                SurveyUserInput survey = (SurveyUserInput) obj;
+                if (survey != null) {
                     Survey.submitUserResponseOffline(context, survey, this, Constants.ApiHitType.logUser);
                 }
                 break;
             case logUser:
-                SurveyUserInput survey1 = (SurveyUserInput)obj;
-                LogUserDBRepo.deleteSentSurveyFromDB(context,new Integer[]{survey1.get_id()},this, Constants.ApiHitType.deleteEventsFromDB);
+                SurveyUserInput survey1 = (SurveyUserInput) obj;
+                LogUserDBRepo.deleteSentSurveyFromDB(context, new Integer[]{survey1.get_id()}, this, Constants.ApiHitType.deleteEventsFromDB);
                 break;
             case deleteSurveyFromDB:
                 checkOffLineSurvey();
