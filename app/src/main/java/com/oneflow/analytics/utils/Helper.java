b670a97e393d15e76df35673f0c3100b265b3fe3
amitskybox <amit@tryskybox.com>
2021-11-24 16:40:38 +0530

    - log printer

--- app/src/main/java/com/oneflow/analytics/utils/Helper.java
+++ app/src/main/java/com/oneflow/analytics/utils/Helper.java
@@ -66,7 +66,7 @@ public class Helper {
     //static boolean debug = commanEnable;
     //static boolean error = false;
     static boolean builds = false;
-    static boolean printLogs = false;
+    static boolean printLogs = true;
     public static String headerKey = "";
 
     public static String gpsProviderInfo;
