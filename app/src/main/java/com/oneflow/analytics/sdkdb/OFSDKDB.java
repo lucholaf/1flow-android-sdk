1a4f377dd56e4f9c44ad66261489e2c0a02d6427
amitskybox <amit@tryskybox.com>
2021-12-20 18:06:00 +0530

    - DB Version update

--- app/src/main/java/com/oneflow/analytics/sdkdb/OFSDKDB.java
+++ app/src/main/java/com/oneflow/analytics/sdkdb/OFSDKDB.java
@@ -21,7 +21,7 @@ import com.oneflow.analytics.sdkdb.survey.OFSubmittedSurveyDAO;
 import com.oneflow.analytics.sdkdb.survey.OFSubmittedSurveysTab;
 import com.oneflow.analytics.utils.OFConstants;
 
-@Database(entities = {OFRecordEventsTab.class, OFAddUserResultResponse.class, OFSubmittedSurveysTab.class, OFSurveyUserInput.class},version = 4)
+@Database(entities = {OFRecordEventsTab.class, OFAddUserResultResponse.class, OFSubmittedSurveysTab.class, OFSurveyUserInput.class},version = 5)
 @TypeConverters({OFMapConverter.class, OFDataConverterLocation.class, OFDataConverterDevice.class, OFSurveyUserResponseChildConverter.class})
 public abstract class OFSDKDB extends RoomDatabase {
 
