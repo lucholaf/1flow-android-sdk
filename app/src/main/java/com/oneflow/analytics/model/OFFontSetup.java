a79ce243caed9ac3dce061866aeeefd0b1bf30c0
amitskybox <amit@tryskybox.com>
2021-12-23 09:14:59 +0530

    - font setup class constructor added

--- app/src/main/java/com/oneflow/analytics/model/OFFontSetup.java
+++ app/src/main/java/com/oneflow/analytics/model/OFFontSetup.java
@@ -11,19 +11,24 @@ public class OFFontSetup {
     @SerializedName("fontSize")
     Float fontSize;
 
+    public OFFontSetup(Typeface face, Float size){
+        this.typeface = face;
+        this.fontSize = size;
+
+    }
     public Typeface getTypeface() {
         return typeface;
     }
 
-    public void setTypeface(Typeface typeface) {
+   /* public void setTypeface(Typeface typeface) {
         this.typeface = typeface;
-    }
+    }*/
 
     public Float getFontSize() {
         return fontSize;
     }
 
-    public void setFontSize(Float fontSize) {
+    /*public void setFontSize(Float fontSize) {
         this.fontSize = fontSize;
-    }
+    }*/
 }
