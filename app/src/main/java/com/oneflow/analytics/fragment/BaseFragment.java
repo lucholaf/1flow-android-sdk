1bf3bb73707643013f8143faebc3f97ec6d9b53c
amitskybox <amit@tryskybox.com>
2022-07-10 13:42:27 +0530

    - ProgressBar and close btn improvement

--- app/src/main/java/com/oneflow/analytics/fragment/BaseFragment.java
+++ app/src/main/java/com/oneflow/analytics/fragment/BaseFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import com.oneflow.analytics.OFSDKBaseActivity;
 import com.oneflow.analytics.OFSurveyActivityBottom;
+import com.oneflow.analytics.OFSurveyActivityFullScreen;
 import com.oneflow.analytics.model.survey.OFSDKSettingsTheme;
 import com.oneflow.analytics.utils.OFHelper;
 
@@ -24,7 +25,7 @@ public class BaseFragment extends Fragment {
     LinearLayout waterMarkLayout;
 
     public void transitActive() {
-        int colorFrom = OFHelper.manipulateColor(Color.parseColor(sa.themeColor),0.5f);//getResources().getColor(R.color.ratings_focused);
+        int colorFrom = OFHelper.manipulateColor(Color.parseColor(sa.themeColor), 0.5f);//getResources().getColor(R.color.ratings_focused);
         int colorTo = Color.parseColor(sa.themeColor);
         ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
         colorAnimation.setDuration(250); // milliseconds
@@ -42,7 +43,7 @@ public class BaseFragment extends Fragment {
 
     public void transitInActive() {
         int colorFrom = Color.parseColor(sa.themeColor);
-        int colorTo = OFHelper.manipulateColor(Color.parseColor(sa.themeColor),0.5f);//getResources().getColor(R.color.ratings_focused);
+        int colorTo = OFHelper.manipulateColor(Color.parseColor(sa.themeColor), 0.5f);//getResources().getColor(R.color.ratings_focused);
         ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
         colorAnimation.setDuration(250); // milliseconds
         colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -59,47 +60,54 @@ public class BaseFragment extends Fragment {
 
     public void handleWaterMarkStyle(OFSDKSettingsTheme theme) {
 
-
-        if (theme.getRemove_watermark()) {
-             waterMarkLayout.setVisibility(View.GONE);
-        } else {
-            waterMarkLayout.setVisibility(View.VISIBLE);
-        }
-        int colorAlpha = OFHelper.manipulateColor(Color.parseColor(OFHelper.handlerColor(theme.getText_color())), 0.1f);
-        GradientDrawable gd = (GradientDrawable) waterMarkLayout.getBackground();
-        waterMarkLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String waterMark = "https://1flow.app/?utm_source=1flow-android-sdk&utm_medium=watermark&utm_campaign=real-time+feedback+powered+by+1flow";//https://www.notion.so/Powered-by-1Flow-logo-should-link-to-website-c186fca5220e41d19f420dd871f9696d";
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(waterMark));
-                startActivity(browserIntent);
-            }
-        });
-        waterMarkLayout.setOnTouchListener(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                switch (event.getAction()) {
-
-                    case MotionEvent.ACTION_DOWN:
-                        gd.setColor(colorAlpha);
-                        break;
-
-                    case MotionEvent.ACTION_MOVE:
-                        // touch move code
-                        //Helper.makeText(mContext,"Moved",1);
-                        break;
-
-                    case MotionEvent.ACTION_UP:
-
-                        gd.setColor(null);
-
-
-                        break;
+        try {
+            if (sa instanceof OFSurveyActivityFullScreen) {
+                waterMarkLayout.setVisibility(View.GONE);
+            } else {
+                if (theme.getRemove_watermark()) {
+                    waterMarkLayout.setVisibility(View.GONE);
+                } else {
+                    waterMarkLayout.setVisibility(View.VISIBLE);
                 }
-                return false;
+                int colorAlpha = OFHelper.manipulateColor(Color.parseColor(OFHelper.handlerColor(theme.getText_color())), 0.1f);
+                GradientDrawable gd = (GradientDrawable) waterMarkLayout.getBackground();
+                waterMarkLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String waterMark = "https://1flow.app/?utm_source=1flow-android-sdk&utm_medium=watermark&utm_campaign=real-time+feedback+powered+by+1flow";//https://www.notion.so/Powered-by-1Flow-logo-should-link-to-website-c186fca5220e41d19f420dd871f9696d";
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(waterMark));
+                        startActivity(browserIntent);
+                    }
+                });
+                waterMarkLayout.setOnTouchListener(new View.OnTouchListener() {
+
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+
+                        switch (event.getAction()) {
+
+                            case MotionEvent.ACTION_DOWN:
+                                gd.setColor(colorAlpha);
+                                break;
+
+                            case MotionEvent.ACTION_MOVE:
+                                // touch move code
+                                //Helper.makeText(mContext,"Moved",1);
+                                break;
+
+                            case MotionEvent.ACTION_UP:
+
+                                gd.setColor(null);
+
+
+                                break;
+                        }
+                        return false;
+                    }
+                });
             }
-        });
+        } catch (Exception ex) {
+            OFHelper.e("BaseFragment", "OneFlow watermark error ");
+        }
     }
 }
