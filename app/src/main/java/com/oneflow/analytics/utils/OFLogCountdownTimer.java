d56c578515e77fc598a619c1063c34e70ebb4c90
amit <amit@tryskybox.com>
2022-08-04 01:30:11 +0530

    - Position bug on app going background - Skip unknow type of survey question - placeholder text added. - to get survey passing minVersion in URL

---
+++ app/src/main/java/com/oneflow/analytics/utils/OFLogCountdownTimer.java
@@ -0,0 +1,86 @@
+package com.oneflow.analytics.utils;
+
+import android.content.Context;
+import android.os.CountDownTimer;
+
+import com.oneflow.analytics.model.adduser.OFAddUserResultResponse;
+import com.oneflow.analytics.model.loguser.OFLogUserRequest;
+import com.oneflow.analytics.model.loguser.OFLogUserResponse;
+import com.oneflow.analytics.repositories.OFLogUserDBRepo;
+import com.oneflow.analytics.repositories.OFLogUserRepo;
+import com.oneflow.analytics.sdkdb.OFOneFlowSHP;
+
+public class OFLogCountdownTimer extends CountDownTimer implements OFMyResponseHandler{
+
+    Context mContext;
+    static OFLogCountdownTimer cdt;
+    static int count = 0;
+    public static synchronized OFLogCountdownTimer getInstance(Context context, Long duration, Long interval) {
+        if (cdt == null) {
+            cdt = new OFLogCountdownTimer(context, duration, interval);
+        }
+        return cdt;
+    }
+    private OFLogCountdownTimer(Context context, Long duration, Long interval) {
+        super(duration, interval);
+        this.mContext = context;
+        OFHelper.v("OFLogCountdownTimer", "OneFlow Constructor called");
+    }
+    @Override
+    public void onTick(long millisUntilFinished) {
+        OFHelper.v("OFLogCountdownTimer", "OneFlow onTick called ");
+        OFLogUserRequest lur = new OFOneFlowSHP(mContext).getLogUserRequest();
+        if (lur != null) {
+            OFLogUserRepo.logUser(new OFOneFlowSHP(mContext).getStringValue(OFConstants.APPIDSHP), lur, this, OFConstants.ApiHitType.logUser);
+        }
+    }
+
+    @Override
+    public void onFinish() {
+
+    }
+
+    @Override
+    public void onResponseReceived(OFConstants.ApiHitType hitType, Object obj, Long reserve, String reserved) {
+        switch (hitType) {
+            case logUser:
+                if (obj != null) {
+
+                    OFLogUserResponse logUserResponse = (OFLogUserResponse) obj;
+                    if (logUserResponse != null) {
+                        // replacing current session id and user analytical id
+                        OFOneFlowSHP ofs = new OFOneFlowSHP(mContext);
+                        ofs.storeValue(OFConstants.SHP_LOG_USER_KEY, reserved);//ofs.getLogUserRequest().getSystem_id()); // system id stored for sending next app launch
+                        ofs.clearLogUserRequest();
+                        OFAddUserResultResponse aurr = ofs.getUserDetails();
+                        //setting up new user analytical id
+
+                        aurr.setAnalytic_user_id(logUserResponse.getAnalytic_user_id());
+                        ofs.setUserDetails(aurr);
+                        ofs.storeValue(OFConstants.SESSIONDETAIL_IDSHP, logUserResponse.getSessionId());
+
+                        //storing this to support multi user survey
+                        ofs.storeValue(OFConstants.USERUNIQUEIDSHP, reserved);
+
+                        // mrh.onResponseReceived(hitType,null,0);
+                        OFHelper.v("OneFlow", "OneFlow Log record inserted...");
+
+                        //Updating old submitted surveys with logged user id.
+                        OFLogUserDBRepo.updateSurveyUserId(mContext, this, reserved, OFConstants.ApiHitType.updateSurveyIds);
+                        cdt.cancel();
+                    } else {
+                        if (OFConstants.MODE.equalsIgnoreCase("dev")) {
+                            OFHelper.makeText(mContext, reserved, 1);
+                        }
+                    }
+                } else {
+                    OFHelper.e("OneFlow", "OneFlow subimission failed logUser");
+                   // OFLogCountdownTimer.getInstance(mContext, 3000l, 1000l).start();
+                }
+
+                break;
+            default:
+
+        }
+    }
+}
