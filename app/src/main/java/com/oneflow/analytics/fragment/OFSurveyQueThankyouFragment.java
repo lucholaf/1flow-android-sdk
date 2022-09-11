fcad274e2debd5155e91fd83c891800ef4809b27
amitskybox <amit@tryskybox.com>
2022-09-11 19:14:07 +0530

    - Submit button text dynamic

--- app/src/main/java/com/oneflow/analytics/fragment/OFSurveyQueThankyouFragment.java
+++ app/src/main/java/com/oneflow/analytics/fragment/OFSurveyQueThankyouFragment.java
@@ -82,8 +82,6 @@ public class OFSurveyQueThankyouFragment extends BaseFragment {
 
 
 
-
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
