d7683e0d0b65cfb22f578236d78fb0f972cdc60e
amit <amit@tryskybox.com>
2022-11-17 15:28:54 +0530

    - throttling - duplicate survey name handling

--- app/src/main/java/com/oneflow/analytics/OFProjectKeyActivity.java
+++ app/src/main/java/com/oneflow/analytics/OFProjectKeyActivity.java
@@ -22,8 +22,8 @@ public class OFProjectKeyActivity extends AppCompatActivity {
         registerProject = (OFCustomTextView) findViewById(R.id.register_project);
         registerProject.setOnClickListener(listener);
         //String projectKy = "oneflow_sandbox_oV+xY+hArzT2i4lMP69YZnRBLK1a/qmYW16MboVc208IVjiNKPfHRIylm0rVFgEubtaRuhKMTdlTt5TEuP+8Pw=="; //AmitRepeatTest
-        //String projectKy = "oneflow_sandbox_jYPADflsoaQX2PxbP62NUrqOq+KJOXiwbvT8cGvwgRRU7Y9S37/ZUMvYIAXJg4Y4SVK6kaNCqqLdbcsxqpIobg=="; //AmitRepeatTest
-        String projectKy = "oneflow_sandbox_rO2j1pVVsisXXMNdhLIBeDOk+wrWzLmPztsYoMTOtb+OYs6qND6EsVENHv3IeNjS/Y9pP6rq6s3BzVknNJZMKA=="; //AmitRepeatTest
+        String projectKy = "oneflow_sandbox_jYPADflsoaQX2PxbP62NUrqOq+KJOXiwbvT8cGvwgRRU7Y9S37/ZUMvYIAXJg4Y4SVK6kaNCqqLdbcsxqpIobg=="; //16Nov
+        //String projectKy = "oneflow_sandbox_rO2j1pVVsisXXMNdhLIBeDOk+wrWzLmPztsYoMTOtb+OYs6qND6EsVENHv3IeNjS/Y9pP6rq6s3BzVknNJZMKA=="; //DuplicateEventName
         //String projectKy = "oneflow_sandbox_NSR9CSPdNygqtxXVU39VBKCozzqk3whBUEQ5/PwGh0cXDGp59m3oD99fkvUeHJQZ7u9P8z2pmjqLk4iR25jSSg=="; //flow1
         //String projectKy = "oneflow_prod_YMslXVT1uFOldcBl5kuupFSuLY1yaWkg1lC9lnsZ9jYDvB1KQdRyp4w34VOvMZwlUZ5efuXUWAV5JEizYPzfwA=="; //AmitTestingProject
         //String projectKy = "oneflow_sandbox_hPz4Tfti7FgaKJ+yTdDGgf+OTNdW2czSmdAFMJL40tGbCqDWfswx+2Zy47zGdcax6zwdQRaYJugbfKglb2SLFA=="; //FakeProject
