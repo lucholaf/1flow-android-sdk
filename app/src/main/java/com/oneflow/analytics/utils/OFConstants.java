97ae27d914fca0a8a9932b42a28f48952ffc2a9b
iMac <>
2023-08-18 18:16:56 +0530

    change version name 2023.08.17 and change mode DEV

--- app/src/main/java/com/oneflow/analytics/utils/OFConstants.java
+++ app/src/main/java/com/oneflow/analytics/utils/OFConstants.java
@@ -20,8 +20,8 @@ package com.oneflow.analytics.utils;
 
 public interface OFConstants {
 
-    String currentVersion = "2023.08.18";//2023.07.14;
-    String MODE = "prod";//"dev";//""beta";//
+    String currentVersion = "2023.08.17";//2023.07.14;
+    String MODE = "dev";//""beta";//"prod";
 
     String cacheFileName = "logic-engine.js";
     String DBNAME = "one_flow_db";
