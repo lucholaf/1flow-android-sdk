cf65f738a36eef219e59acc28204832ec89b2f43
iMac <>
2023-09-18 12:13:40 +0530

    update target version and rxjava dependency

--- app/src/main/java/com/oneflow/analytics/utils/OFConstants.java
+++ app/src/main/java/com/oneflow/analytics/utils/OFConstants.java
@@ -20,8 +20,8 @@ package com.oneflow.analytics.utils;
 
 public interface OFConstants {
 
-    String currentVersion = "2023.08.20";//2023.07.14;
-    String MODE = "dev";//"prod";//"beta";//
+    String currentVersion = "2023.09.18";//2023.07.14;
+    String MODE = "prod";//"dev";//"beta";//
 
     String cacheFileName = "logic-engine.js";
     String DBNAME = "one_flow_db";
